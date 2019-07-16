a3d3efff7850fd716dfcd5d2f93e8452d51af375
Miroslav Roman Roson <miroslav.roman.roson@gmail.com>
2019-07-16 08:40:33 +0200

    master: add settings for vs code

---
+++ settings/vs_code_setting.py
@@ -0,0 +1,33 @@
+{
+    "git.ignoreLimitWarning": true,
+    "editor.wordWrap": "wordWrapColumn",
+    "editor.wrappingIndent": "same",
+    "editor.wordWrapColumn": 100,
+    "editor.rulers": [
+        100
+    ],
+    "python.pythonPath": "C:\\Users\\mroson\\.virtualenvs\\env_py37\\Scripts\\python.exe",
+
+    "python.linting.enabled": true,
+    "python.linting.flake8Enabled": true,
+    "python.linting.flake8Args": [
+        "--max-line-length=100"
+
+    ],
+    "python.linting.pep8Enabled": true,
+    "python.linting.pep8Args": [
+        "--max-line-length=100"
+
+   ],
+    "python.linting.pylintEnabled": true,
+    "python.linting.pylintArgs": [
+        "--extension-pkg-whitelist=cv2",
+        "--max-line-length=100"
+
+   ],
+    "python.formatting.autopep8Args": [
+        "--max-line-length=100"
+
+   ],
+    "files.trimTrailingWhitespace": true
+}
